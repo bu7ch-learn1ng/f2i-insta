bc45ab4e2f8ccd2f7c1c23c89d43ab71847b1f71
Rodolphe <dutchr@live.fr>
2023-08-24 15:31:26 +0200

    :sparkles: Adding CRUD methods

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -17,5 +17,30 @@ const createPost = async (req, res) => {
     res.status(500).json({ msg: "Nous avons rencontrer une erreur" });
   }
 };
+const getPost = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const post = await Post.findOne({ _id: id });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ msg: "Nous avons rencontrer une erreur" });
+  }
+};
 
-module.exports = { getAllPosts, createPost };
+const UpdatePost = async (req,res, next) => {
+  const id = req.params.id;
+
+  try {
+    let post = await Post.findOneAndUpdate(id, req.body)
+    res.json(post)
+  } catch (error) {
+    next(error)
+  }
+}
+const deletePost = async(req,res) => {
+  const id = req.params.id
+ await Post.findOneAndRemove({_id: id})
+ res.json({msg:"Le post a bien été supprimé !"})
+}
+module.exports = { getAllPosts, createPost, getPost, UpdatePost, deletePost
+ };
--- routes/postsRoute.js
+++ routes/postsRoute.js
@@ -1,9 +1,18 @@
-const { getAllPosts, createPost } = require("../controllers/PostController");
+const {
+  getAllPosts,
+  createPost,
+  getPost,
+  UpdatePost,
+  deletePost
+} = require("../controllers/PostController");
 
 const router = require("express").Router();
 
 router.get("/all", getAllPosts);
 
 router.post("/new", createPost);
-
+router.route('/:id')
+.get(getPost)
+.put(UpdatePost)
+.delete(deletePost)
 module.exports = router;
