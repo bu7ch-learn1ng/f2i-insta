fb79c5defe78b51911a4349d4b576af7d7b2b0df
Rodolphe <dutchr@live.fr>
2023-08-24 14:31:02 +0200

    App connected to mongoDB

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
+const mongoose = require('mongoose')
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/f2i-insta-flex');
+  console.log(`📚[Database] is connected to MongoDB`)
+}
 app.use('/', (req, res) => {
   console.log('🐲')
 })
